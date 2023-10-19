3906f8dfc34fc1f2b0337355f62ac84dcf72fe1f
Digvijay Singh Shekhawat <72626219+DIGVI962@users.noreply.github.com>
2023-10-20 00:24:35 +0530

    Add files via upload

---
+++ Solution.py
@@ -0,0 +1,64 @@
+
+import time
+
+class TrieNode:
+    def __init__(self, value = None) -> None:
+        self.value = value
+        self.child = {}
+        self.eos = False
+
+class SolutionTrietree:
+    def __init__(self) -> None:
+        self.root = TrieNode()
+        self.buffer = []
+
+    def insertion(self, word) -> None:
+        currNode = self.root
+
+        for i in range(len(word)):
+            character = word[i]
+            if currNode.eos:
+                self.buffer.append((word, word[i:]))
+            if character not in currNode.child:
+                currNode.child[character] = TrieNode(character)
+            currNode = currNode.child[character]
+        currNode.eos = True
+
+    def is_compounded(self, record) -> bool:
+        currNode = self.root
+        temp = []
+        suffix = record[1]
+        for i in range(len(suffix)):
+            character = suffix[i]
+            if currNode.eos:
+                temp.append((record[0], suffix[i:]))
+            if character not in currNode.child:
+                self.buffer.extend(temp)
+                return False
+            currNode = currNode.child[character]
+        if not currNode.eos:
+            self.buffer.extend(temp)
+        return currNode.eos
+
+    def longest_compounded_word(self) -> tuple:
+        longest, second_longest = '', ''
+        for record in self.buffer:
+            if ((len(record[0]) > len(second_longest)) and self.is_compounded(record)):
+                if (len(longest) < len(record[0])):
+                    longest, second_longest = record[0], longest
+                else:
+                    second_longest = record[0]
+        return (longest, second_longest)
+   
+
+if __name__ == '__main__':
+    begin = time.time()
+    trie = SolutionTrietree()
+    with open('Input_02.txt') as f:
+        for line in f.readlines():
+            trie.insertion(line.strip())
+    longest, second_longest = trie.longest_compounded_word()
+    end = time.time()
+    print('Longest Compounded Word: ', longest)
+    print('Second Longest Compounded Word: ', second_longest)
+    print(f'Execution time: {(end - begin)*1000} milliseconds')
